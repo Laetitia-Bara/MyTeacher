705c4c81f1d5ed5566fb128958f00c0f629bb12b
Timothee <dray.timothee@gmail.com>
2026-03-03 14:13:10 +0100

    Sauvegarde pour pull

--- frontend/components/FicheEleveProf.js
+++ frontend/components/FicheEleveProf.js
@@ -7,6 +7,30 @@ function FicheEleveProf () {
     return (
         <div className={styles.body}>
             <HeaderProf />
+
+            <h1 className={styles.titre}>
+                Patrick Richard
+            </h1>
+
+            <div className={styles.container}>
+                <div className={styles.left}>
+                    <fieldset className={styles.content}>
+                        <legend className={styles.title}>Identité</legend>
+                        
+                    </fieldset>
+
+                    <fieldset className={styles.content}>
+                        <legend className={styles.title}>Suivi des cours</legend>
+                        Contenu de la div
+                    </fieldset>
+                </div>
+
+                <fieldset className={styles.right}>
+                    <legend className={styles.title}>Historique de paiement</legend>
+                    Contenu de la div
+                </fieldset>
+            </div>
+
             <FooterProf />
         </div>
     );
