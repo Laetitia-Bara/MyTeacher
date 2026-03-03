69e90e8c384f5ff72877caf1980f0bd05627e799
Timothee <dray.timothee@gmail.com>
2026-03-03 10:49:42 +0100

    Design Signup Prof

--- frontend/components/SignupProf.js
+++ frontend/components/SignupProf.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-
 import FooterProf from './FooterProf';
 import styles from '../styles/SignupProf.module.css';
 
@@ -10,7 +9,15 @@ function SignupProf() {
                 <Image className={styles.logo} src="/LogoMT.ico" alt="Logo" width={65} height={50}/>
             </div>
 
-            <h1>Bienvenue sur MyTeacher</h1>
+            <h1 className={styles.titre}>Bienvenue sur MyTeacher</h1>
+
+            <div className={styles.container}>
+                <input className={styles.input} type="text" placeholder='Email'/>
+                <input className={styles.input} type="password" placeholder='Mot de passe'/>
+                <button className={styles.boutonBleu}>Se connecter</button>
+            </div>
+
+            <button className={styles.boutonVert}>Pas encore de compte ? Par ici !</button>
 
             <FooterProf />
         </div>
