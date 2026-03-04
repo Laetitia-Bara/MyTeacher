166f8bff83b07fe7d602459bd7cc20a8c9ee22be
Nicolas <nicolas.dumont.important@gmail.com>
2026-03-04 17:30:46 +0100

    getInvoice route with modalite

--- backend/routes/invoices.js
+++ backend/routes/invoices.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 require("../models/connection");
 const Invoice = require("../models/invoices");
+const Student = require("../models/students");
 const { checkBody } = require("../modules/checkBody");
 const authMiddleware = require("../middlewares/auth");
 const requireRole = require("../middlewares/requireRole");
@@ -16,26 +17,29 @@ router.get('/getInvoices',
     function (req, res) {
     let invoices = []
     Invoice.find({teacher: req.user.userId})
-    .populate(({
+    .populate({
          path    : 'student',
          populate: 'user'
-    }))
+    })
     .then(data => {
         if(data.length > 0)
         {
-            for(let obj of data)
-            {
-                invoices.push({
-                    firstName: obj.student.user.firstName,
-                    lastName: obj.student.user.lastName,
-                    period: obj.period,
-                    label: obj.label,
-                    amount: obj.amount,
-                    status: obj.status,
-                    createdAt : obj.createdAt
-                })
-            }
-            res.json({result: true, invoices: invoices})
+            Student.find({ teacher: req.user.userId }).then((studentInfo) => {
+                for(let obj of data)
+                {
+                    let modalite = studentInfo.find((element) => String(element._id) === String(obj.student._id)).subscription.modalite
+                    invoices.push({
+                        firstName: obj.student.user.firstName,
+                        lastName: obj.student.user.lastName,
+                        period: obj.period,
+                        label: obj.label,
+                        amount: obj.amount,
+                        status: obj.status,
+                        createdAt : obj.createdAt,
+                        modalite: modalite //modalite de paiement dans student
+                    })
+                }
+            }).then(() => {res.json({result: true, invoices: invoices})})
         }else{res.json({result: false, error : "No invoices found"})}
     })
 })
