1cbcd6d9cdb0c3cec5386c95e37467ade0d6e4a9
Laetitia <bara.laetitia@gmail.com>
2026-03-05 09:23:04 +0100

    modif api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,21 +1,15 @@
-console.log("API_URL DEBUG", {
-  NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  NODE_ENV: process.env.NODE_ENV,
-});
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const isDev = process.env.NODE_ENV === "development";
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_BACKEND_URL is missing");
+}
 
-const API_URL = isDev
-  ? process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000"
-  : process.env.NEXT_PUBLIC_BACKEND_URL;
-
-if (!API_URL) throw new Error("NEXT_PUBLIC_BACKEND_URL is missing");
 export async function api(path, { method = "GET", body } = {}) {
   const res = await fetch(`${API_URL}${path}`, {
     method,
     headers: body ? { "Content-Type": "application/json" } : undefined,
     body: body ? JSON.stringify(body) : undefined,
-    credentials: "include", // IMPORTANT: cookie JWT
+    credentials: "include", // cookie JWT
   });
 
   const data = await res.json().catch(() => ({}));
