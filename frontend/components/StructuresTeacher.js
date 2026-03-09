fe6b3f8aa08336059ead28763d229ae5646af756
Thomas MENAGE <thomas.menage@hei.fr>
2026-03-09 17:49:44 +0100

    Structure page ongoing

---
+++ frontend/components/StructuresTeacher.js
@@ -0,0 +1,197 @@
+import Head from "next/head";
+import HeaderTeacher from "./HeaderTeacher";
+import FooterTeacher from "./FooterTeacher";
+import StructureCard from "./StructureCard";
+import ModalAddStructure from "./ModalAddStructure";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import styles from "../styles/StructuresTeacher.module.css";
+
+const dataStructures = [
+  {
+    _id: 1,
+    name: "Auto-entreprise",
+    weekWorkLoad: 10,
+    feesPlace: 30,
+    accountability: 50,
+    income: 600,
+  },
+  {
+    _id: 2,
+    name: "Conservatoire",
+    weekWorkLoad: 8,
+    feesPlace: 0,
+    accountability: 0,
+    income: 400,
+  },
+];
+
+function StructuresTeacher() {
+  const [structuresData, setStructuresData] = useState([]);
+  const [modalAddStructure, setModalAddStructure] = useState(false);
+  const [addFlag, setAddFlag] = useState(false);
+
+  useEffect(() => {
+    // (async () => {
+    //   // Fetch structures
+    //   try {
+    //     const response = await fetch(
+    //       `${process.env.NEXT_PUBLIC_BACKEND_URL}/structures/getStructures`,
+    //       {
+    //         method: "GET",
+    //         credentials: "include",
+    //       },
+    //     );
+    //     const data = await response.json();
+    //     console.log("data structures", data);
+    //     // Version dès que backend ok
+    //     data.result
+    //       ? setStructuresData(data.structures)
+    //       : console.log(data.error);
+    //   } catch (error) {
+    //     console.error("Error fetching data:", error);
+    //   }
+    // })();
+
+    //En attendant que back fonctionne
+    setStructuresData(dataStructures);
+  }, [addFlag]);
+
+  const deleteStructure = async (comingProps) => {
+    console.log("Supprimer structure", comingProps._id);
+    // Fetch delete ressource
+    // try {
+    //   const response = await fetch(
+    //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/structures/deleteStructure/${comingProps._id}`,
+    //     {
+    //       method: "DELETE",
+    //       credentials: "include",
+    //     },
+    //   );
+
+    //   const data = await response.json();
+    //   console.log("Data structures fetched:", data);
+    //   // Version dès que backend ok
+    //   data.result
+    //     ? setStructuresData((ress) =>
+    //         ress.filter((r) => r._id !== comingProps._id),
+    //       )
+    //     : console.log(data.error);
+    // } catch (error) {
+    //   console.error("Error fetching data:", error);
+    // }
+
+    // En attendant que back ok
+    dataStructures = dataStructures.filter(
+      (stru) => stru._id !== comingProps._id,
+    );
+    setAddFlag(!addFlag);
+  };
+
+  const handleAddStructure = async (newStructure) => {
+    console.log("Ajouter structure", newStructure);
+    // if (
+    //   newStructure.name !== "" &&
+    //   newStructure.weekHourLoad !== "" &&
+    //   newStructure.feesPlace !== "" &&
+    //   newStructure.accountability !== "" &&
+    //   newStructure.income !== ""
+    // ) {
+    //   try {
+    //     const response = await fetch(
+    //       `${process.env.NEXT_PUBLIC_BACKEND_URL}/structures/add`,
+    //       {
+    //         method: "POST",
+    //         credentials: "include",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //           //   // Authorization: `Bearer ${token}`,
+    //         },
+    //         body: {
+    //           name: newStructure.name,
+    //           weekHourLoad: newStructure.weekHourLoad,
+    //           feesPlace: newStructure.feesPlace,
+    //           accountability: newStructure.accountability,
+    //           income: newStructure.income,
+    //         },
+    //       },
+    //     );
+
+    //     const data = await response.json();
+    //     console.log("Data structures fetched:", data);
+
+    //     if (data.result) {
+    //       alert("Structure ajoutée !");
+    //       setAddFlag(!addFlag);
+    //     } else {
+    //       alert(`Erreur : ${data.error}`);
+    //     }
+    //   } catch (error) {
+    //     console.error("Error adding structure :", error);
+    //   }
+    // } else {
+    //   console.log("Input data missing");
+    // }
+
+    //En attendant que backend ok
+    dataStructures.push({
+      _id: Date.now(),
+      name: newStructure.name,
+      weekWorkLoad: newStructure.weekWorkLoad,
+      feesPlace: newStructure.feesPlace,
+      accountability: newStructure.accountability,
+      income: newStructure.income,
+    });
+
+    setAddFlag(!addFlag);
+  };
+
+  const structures = structuresData?.map((data, i) => (
+    <StructureCard
+      key={i}
+      _id={data._id}
+      name={data.name}
+      weekWorkLoad={data.weekWorkLoad}
+      feesPlace={data.feesPlace}
+      accountability={data.accountability}
+      income={data.income}
+      deleteFct={deleteStructure}
+    />
+  ));
+
+  return (
+    <div className={styles.content}>
+      <Head>
+        <title>MyTeacher - Structures </title>
+      </Head>
+      <HeaderTeacher />
+      <main className={styles.main}>
+        <div className={styles.titlePage}>
+          <p className={styles.title}>MES STRUCTURES</p>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.structuresList}>
+            <p className={styles.subtitle}>Mes structures</p>
+            <button
+              className={styles.btn}
+              onClick={() => setModalAddStructure(true)}
+            >
+              <span className={styles.addText}>+ Ajouter</span>
+            </button>
+            {structures}
+          </div>
+        </div>
+      </main>
+      <FooterTeacher />
+      {modalAddStructure && (
+        <ModalAddStructure
+          onClose={() => setModalAddStructure(false)}
+          addStructureFct={handleAddStructure}
+        />
+      )}
+    </div>
+  );
+}
+
+export default StructuresTeacher;
