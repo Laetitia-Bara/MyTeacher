6b9e6aacdc50beac527c39cb90d4121d2b3f85f5
Laetitia <bara.laetitia@gmail.com>
2026-03-12 00:19:24 +0100

    Homogen css dash Student vs Teacher

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,6 @@
-import DashboardTeacher from "../components/DashboardTeacher";
 import Signin from "../components/Signin";
-import DashboardStudent from "../components/DashboardStudent";
-
 
 function Index() {
-  
   return <Signin />;
 }
 
