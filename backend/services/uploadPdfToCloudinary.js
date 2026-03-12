36d4b5fb341fa9e1a6e116ba9f667b562e30f7d2
Laetitia <bara.laetitia@gmail.com>
2026-03-12 19:32:43 +0100

    ajout + connexion PDF invoice + cloudinary

---
+++ backend/services/uploadPdfToCloudinary.js
@@ -0,0 +1,23 @@
+const streamifier = require("streamifier");
+const cloudinary = require("./cloudinary");
+
+function uploadPdfToCloudinary(buffer, publicId) {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "raw",
+        folder: "myteacher/invoices",
+        public_id: publicId,
+        format: "pdf",
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      },
+    );
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+}
+
+module.exports = uploadPdfToCloudinary;
